7b8e2ef3ce8d7efea32aad2b2c1caa89a586a6aa
Borys Medvediev <medvediev.b@gmail.com>
2026-01-28 21:09:36 +0100

    feat(web): email input

--- apps/web/app/checkout/page.tsx
+++ apps/web/app/checkout/page.tsx
@@ -15,16 +15,20 @@ export default function CheckoutPage() {
 
           <Text className="mt-2">Please review your order before proceeding to payment.</Text>
 
-          <FormField label="First Name" className="mt-4">
+          <FormField id="firstName" label="First Name" className="mt-4">
             <Input placeholder="e.g. Stephen" />
           </FormField>
 
-          <FormField label="Last Name" className="mt-2">
+          <FormField id="lastName" label="Last Name" className="mt-2">
             <Input placeholder="e.g. King" />
           </FormField>
 
-          <FormField label="Phone Number" className="mt-2">
+          <FormField id="phoneNumber" label="Phone Number" className="mt-2">
             <PhoneInput placeholder="e.g. +49 1234 567890" />
+          </FormField>
+
+          <FormField id="email" label="Email Address" className="mt-2">
+            <Input type="email" inputMode="email" placeholder="e.g. stephen.king@example.com" />
           </FormField>
         </Surface>
       </div>
